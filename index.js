4614b462fa6366dfa954e516c0610c316451d0d8
Darah <darashalabi@gmail.com>
2020-09-18 12:46:19 +0300

    api and req routes

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 
 require('dotenv').config();
 const mongoose= require('mongoose');
-const server= require('./lib/server.js');
+const server= require('./src/server.js');
 mongoose.connect(process.env.MONGODB_URI, {
     useNewUrlParser:true,
     useUnifiedTopology:true,
--- src/server.js
+++ src/server.js
@@ -1,11 +1,15 @@
 'use strict';
 
 const express= require('express');
+const morgan= require('morgan');
 const cors= require('cors');
+const apiRouter= require('../lib/api/api-v1.js');
 
 const app= express();
+app.use(morgan('dev'));
 app.use(express.json());
 app.use(cors());
+app.use('/api/v1', apiRouter);
 
 module.exports= {
     server: app,
